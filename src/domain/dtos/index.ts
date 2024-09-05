93645acdeb9527a9c9bdcafa7a9a766822bda09c
DennisCeballos <86538156+DennisCeballos@users.noreply.github.com>
2024-09-04 21:42:04 -0500

    hotfix: creacion de archivo index para exportar dtos

---
+++ src/domain/dtos/index.ts
@@ -0,0 +1,8 @@
+export * from './users/create-user.dto';
+export * from './users/update-user.dto';
+
+export * from './gifts/create-gift.dto';
+export * from './gifts/update-gift.dto';
+
+export * from './groups/create-group.dto';
+export * from './groups/update-group.dto';
