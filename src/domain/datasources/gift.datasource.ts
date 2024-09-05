99da4f67a6bd7f314d37c820905681ba6b354c5f
DennisCeballos <86538156+DennisCeballos@users.noreply.github.com>
2024-09-04 21:57:10 -0500

    feat: creacion de datasources

---
+++ src/domain/datasources/gift.datasource.ts
@@ -0,0 +1,10 @@
+import { CreateGiftDto, UpdateGiftDto } from "../dtos";
+import { GiftEntity } from "../entities/gift.entity";
+
+export abstract class GiftDatasource {
+    abstract create(createGiftDto: CreateGiftDto): Promise<GiftEntity>;
+    abstract getAll(): Promise<GiftEntity[]>;
+    abstract findById(id:number): Promise<GiftEntity>;
+    abstract updateById(updateGiftDto: UpdateGiftDto): Promise<GiftEntity>;
+    abstract deleteById(id:number): Promise<GiftEntity>;
+}
