54ca3e42b06d5f48c38ef1be9f3f163bee5f85fc
DennisCeballos <86538156+DennisCeballos@users.noreply.github.com>
2024-09-06 00:32:31 -0500

    feat: archivo index de domain
    Fin de generacion de carpeta domain

---
+++ src/domain/index.ts
@@ -0,0 +1,36 @@
+// ALL User
+export * from './datasources/user.datasource'
+export * from './dtos'
+export * from './entities/user.entity'
+export * from './repositories/user.repository'
+
+export * from './use-cases/user/create-user'
+export * from './use-cases/user/delete-user'
+export * from './use-cases/user/get-user'
+export * from './use-cases/user/list-user'
+export * from './use-cases/user/update-user'
+
+// ALL Gift
+export * from './datasources/gift.datasource'
+export * from './dtos'
+export * from './entities/gift.entity'
+export * from './repositories/gift.repository'
+
+export * from './use-cases/gift/create-gift'
+export * from './use-cases/gift/delete-gifts'
+export * from './use-cases/gift/get-gift'
+export * from './use-cases/gift/list-gift'
+export * from './use-cases/gift/update-gifts'
+
+
+// ALL Group 
+export * from './datasources/group.datasource'
+export * from './dtos'
+export * from './entities/group.entity'
+export * from './repositories/group.repository'
+
+export * from './use-cases/group/create-group'
+export * from './use-cases/group/delete-group'
+export * from './use-cases/group/get-group'
+export * from './use-cases/group/list-group'
+export * from './use-cases/group/update-group'
