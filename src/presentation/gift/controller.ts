dfd556ec8586ac9d733d6d1ff0cadb395d014b6c
DennisCeballos <86538156+DennisCeballos@users.noreply.github.com>
2024-10-13 01:58:42 -0500

    feat: creacion de archivos presentation/gift/*

---
+++ src/presentation/gift/controller.ts
@@ -0,0 +1,58 @@
+import { Request, Response } from "express";
+import { prisma } from "../../data/postgres";
+import { CreateGiftDto, UpdateGiftDto } from "../../domain";
+import { CreateGift, DeleteGift, ListGifts, GetGift, GiftRepository, UpdateGift } from "../../domain";
+
+export class GiftController {
+
+    constructor(
+        private readonly giftRepository: GiftRepository,
+    ){}
+
+    public listGifts = (req:Request, res:Response) => {
+        new ListGifts(this.giftRepository)
+            .execute()
+            .then(gifts => res.json(gifts))
+            .catch(error => res.status(400).json({error}));
+    };
+
+    public getGiftById = (req:Request, res:Response) => {
+        const id = +req.params.id;
+
+        new GetGift(this.giftRepository)
+            .execute(id)
+            .then(gift => res.json(gift))
+            .catch(error=>res.status(400).json({error}))
+    }
+
+    public createGift = (req:Request, res:Response) => {
+        const [error, createGiftDto] = CreateGiftDto.create(req.body);
+        if (error) return res.status(400).json({error});
+
+        new CreateGift(this.giftRepository)
+            .execute( createGiftDto! )
+            .then(gift => res.json(gift))
+            .catch(error=>res.status(400).json({error}));
+    };
+
+    public updateGift = (req:Request, res:Response )=>{
+        const id =+req.params.id;
+
+        const [error, updateGiftDto] = UpdateGiftDto.create({...req.body, id});
+        if(error) return res.status(400).json({error});
+
+        new UpdateGift(this.giftRepository)
+            .execute(updateGiftDto!)
+            .then(gift => res.json(gift))
+            .catch(error=>res.status(400).json({error}));
+    };
+
+    public deleteGift = (req: Request, res: Response) => {
+        const id = + req.params.id;
+        new DeleteGift(this.giftRepository)
+            .execute(id)
+            .then(gift => res.json(gift))
+            .catch(error=>res.status(400).json({error}))
+    };
+
+}
---
+++ src/presentation/gift/routes.ts
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import { GiftController, GiftController } from "./controller";
+import { GiftDatasourceImpl } from "../../infrastructure/datasource/gift.data.impl";
+import { GiftRepositoryImpl } from "../../infrastructure/repositories/gift.repository.impl";
+
+export class GiftRoutes {
+
+    static get routes(): Router {
+
+        const router = Router();
+
+        const datasource = new GiftDatasourceImpl();
+
+        const giftRepository = new GiftRepositoryImpl(datasource);
+
+        const giftController = new GiftController(giftRepository);
+
+        router.get('/', giftController.listGifts);
+        router.get('/:id', giftController.getGiftById );
+        
+        router.post('/', giftController.createGift );
+        router.put('/:id', giftController.updateGift );
+        router.delete('/:id', giftController.deleteGift );
+
+        return router;
+    }
+}
